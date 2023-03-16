719d09a18b76f184229b455544b68310563e26b9
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-03-15 20:56:02 -0400

    moved nav and head to _app

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,37 @@
 import Link from 'next/link';
+import { useMediaQuery } from 'react-responsive';
+import { MenuOutlined } from '@ant-design/icons';
 
-function Nav() {
-  return (
-    <div>
-      NAVBAR
-    </div>
-  )
-}
 
-export default Nav
+const Nav = () => {
+	const isMobile = useMediaQuery({
+		query: '(max-width: 600px)',
+	});
+
+	return (
+		<>
+			<header className='header-wrapper'>
+				<div className='header-content'>
+					{isMobile ? (
+						<div className='nav-mobile'>
+							<h1>Fantasy Football</h1>
+							<MenuOutlined />
+						</div>
+					) : (
+						<div className='nav-desktop'>
+							<h1>Fantasy Football</h1>
+							<span className='nav-desktop-positions'>
+								{/* <Link>QB</Link>
+								<Link>RB</Link>
+								<Link>WR</Link>
+								<Link>TE</Link> */}
+							</span>
+						</div>
+					)}
+				</div>
+			</header>
+		</>
+	);
+};
+
+export default Nav;
