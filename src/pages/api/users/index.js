26f56b1d3aa4726f4fb9a090aa78a9fba46fee8c
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-03-21 21:21:18 -0400

    user routes

---
+++ src/pages/api/users/index.js
@@ -0,0 +1,42 @@
+import connectDB from '../../../../lib/connectDB';
+import { hash, genSalt, compare } from 'bcryptjs';
+import { getToken } from 'next-auth/jwt';
+
+import User from '../../../../models/User';
+
+export default async function handler(req, res) {
+	const { method } = req;
+	const { username, password } = req.body;
+
+	await connectDB();
+
+	switch (method) {
+		case 'GET':
+			try {
+				// find user
+				const user = await User.findByOne({ username });
+				if (user && (await compare(password, user.password))) {
+					res.status(200).json({
+						success: true,
+						data: user,
+					});
+				}
+			} catch (error) {
+				res.status(401);
+				throw new Error('Invalid Credentials');
+			}
+			break;
+		case 'POST':
+			try {
+				// create new model
+				const user = await User.create(req.body);
+				res.status(201).json({ success: true, data: user });
+			} catch (error) {
+				res.staus(400).json({ success: false });
+			}
+			break;
+		default:
+			res.status(400).json({ success: false });
+			break;
+	}
+}
