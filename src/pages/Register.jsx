f768f2dbaf40c27622b2907557911d1001efe42c
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-03-21 21:22:11 -0400

    register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,12 @@
+import RegisterForm from "../../components/RegisterForm"
+
+const Register = () => {
+  return (
+    <div className="page-container">
+      <h2>Create Account</h2>
+      <RegisterForm />
+    </div>
+  )
+}
+
+export default Register
