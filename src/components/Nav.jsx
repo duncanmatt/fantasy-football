c1ac98cae0a56c9dd218eb2ae9bd43ae31f9f039
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-04-18 17:10:58 -0700

    sleeper styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,103 +1,95 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Link from 'next/link';
-import { Divider } from 'antd';
+import Link from "next/link";
+import { Divider } from "antd";
 import {
-	MenuOutlined,
-	UserOutlined,
-	CloseOutlined,
-	RightOutlined,
-} from '@ant-design/icons';
-import styles from '../styles/Nav.module.css';
+  MenuOutlined,
+  UserOutlined,
+  CloseOutlined,
+  RightOutlined,
+} from "@ant-design/icons";
+import styles from "../styles/Nav.module.css";
 
 function MobileNav() {
-	const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-	const showMenu = () => setMenuOpen(!menuOpen);
+  const showMenu = () => setMenuOpen(!menuOpen);
 
-	return (
-		<div className={styles.mobileWrapper}>
-			{menuOpen ? (
-				<>
-					<div className={styles.mobile}>
-						<Link href='/'>
-							<h1>Football</h1>
-						</Link>
-						<span className={styles.actions}>
-							<Link
-								href='/Login'
-								className={styles.login}></Link>
-							<CloseOutlined onClick={showMenu} />
-						</span>
-					</div>
-					<nav className={styles.menu}>
-						<span className={styles.mobileLinks}>
-							<Link href='/QBs'>
-								QUARTER BACKS <RightOutlined />
-							</Link>
-							<Divider />
-							<Link href='/RBs'>
-								RUNNING BACKS <RightOutlined />
-							</Link>
-							<Divider />
-							<Link href='/WRs'>
-								WIDE RECEIVERS <RightOutlined />
-							</Link>
-							<Divider />
-							<Link href='/TEs'>
-								TIGHT ENDS <RightOutlined />
-							</Link>
-							<Divider />
-						</span>
-					</nav>
-				</>
-			) : (
-				<div className={styles.mobile}>
-					<Link href='/'>
-						<h1>Football</h1>
-					</Link>
-					<span className={styles.actions}>
-						<Link
-							href='/Login'
-							className={styles.login}>
-							<UserOutlined />
-						</Link>
-						<MenuOutlined onClick={showMenu} />
-					</span>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className={styles.mobileWrapper}>
+      {menuOpen ? (
+        <>
+          <div className={styles.mobile}>
+            <Link href="/">
+              <h1>Offseason Informer</h1>
+            </Link>
+            <span className={styles.actions}>
+              <Link href="/Login" className={styles.login}></Link>
+              <CloseOutlined onClick={showMenu} />
+            </span>
+          </div>
+          <nav className={styles.menu}>
+            <span className={styles.mobileLinks}>
+              <Link href="/QBs">
+                QUARTER BACKS <RightOutlined />
+              </Link>
+              <Divider />
+              <Link href="/RBs">
+                RUNNING BACKS <RightOutlined />
+              </Link>
+              <Divider />
+              <Link href="/WRs">
+                WIDE RECEIVERS <RightOutlined />
+              </Link>
+              <Divider />
+              <Link href="/TEs">
+                TIGHT ENDS <RightOutlined />
+              </Link>
+              <Divider />
+            </span>
+          </nav>
+        </>
+      ) : (
+        <div className={styles.mobile}>
+          <Link href="/">
+            <h1>Offseason Informer</h1>
+          </Link>
+          <span className={styles.actions}>
+            <Link href="/Login" className={styles.login}>
+              <UserOutlined />
+            </Link>
+            <MenuOutlined onClick={showMenu} />
+          </span>
+        </div>
+      )}
+    </div>
+  );
 }
 
 const Nav = () => {
-	return (
-		<>
-			<header className='header-wrapper'>
-				<div className='header-content'>
-					<MobileNav />
-					<div className={styles.desktop}>
-						<h1>
-							<Link
-								className={styles.title}
-								href='/'>
-								Football
-							</Link>
-						</h1>
-						<nav className={styles.links}>
-							<Link href='/QBs'>QB</Link>
-							<Link href='/RBs'>RB</Link>
-							<Link href='/WRs'>WR</Link>
-							<Link href='/TEs'>TE</Link>
-							<Link href='/Login'>
-								<UserOutlined />
-							</Link>
-						</nav>
-					</div>
-				</div>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header className="header-wrapper">
+        <div className="header-content">
+          <MobileNav />
+          <div className={styles.desktop}>
+            <Link className={styles.title} href="/">
+              <h1>Offseason Informer</h1>
+            </Link>
+            <nav className={styles.links}>
+              <Link href="/QBs">QB</Link>
+              <Link href="/RBs">RB</Link>
+              <Link href="/WRs">WR</Link>
+              <Link href="/TEs">TE</Link>
+              <Link href="/Login">
+                <UserOutlined />
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Nav;
--- src/components/SleeperCard.jsx
+++ src/components/SleeperCard.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Card } from "antd";
-import styles from "../styles/Player.module.css";
+import { PlusOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 
@@ -10,8 +9,9 @@ const SleeperCard = ({ player }) => {
   return (
     <Card
       title={player.name}
+      hoverable
       loading={!player ? true : false}
-      extra={<Link href={`/players/${player._id}`}>view</Link>}
+      extra={<Link href={`/players/${player._id}`}><PlusOutlined /></Link>}
     >
       <Meta
         description={player.sleeper[1]}
@@ -21,6 +21,7 @@ const SleeperCard = ({ player }) => {
             alt={player.name}
             width={100}
             height={100}
+            style={{borderRadius: '60%', backgroundColor: 'rgba(0,0,0,0.09)'}}
           />
         }
       />
