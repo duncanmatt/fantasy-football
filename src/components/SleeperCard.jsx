c5edf99b323825f0f6fe31019ccd8376e922a529
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-04-20 15:32:18 -0700

    sleeper card responsive layout

--- src/components/SleeperCard.jsx
+++ src/components/SleeperCard.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Card } from "antd";
+import styles from "../styles/Player.module.css";
 
 const { Meta } = Card;
 
@@ -10,24 +11,27 @@ const SleeperCard = ({ player }) => {
       <Card
         title={player.name}
         hoverable="true"
+        size="small"
         type="inner"
         loading={!player ? true : false}
-        extra={player.position}
+        // extra={player.position}
+        // style={{ height: "400px" }}
       >
         <Meta
-          description={player.sleeper[1]}
+          className={styles.meta}
           avatar={
             <Image
               src={player.imgUrl}
               alt={player.name}
-              width={100}
-              height={100}
+              height={200}
+              width={200}
               style={{
                 borderRadius: "60%",
                 backgroundColor: "rgba(0,0,0,0.09)",
               }}
             />
           }
+          title={player.position} description={player.sleeper[1]}
         />
       </Card>
     </Link>
