58453916168bbc893354f1c84c8663b87b0bb44f
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-04-19 21:59:59 -0700

    sleeper card wrapped in link

--- src/components/SleeperCard.jsx
+++ src/components/SleeperCard.jsx
@@ -1,31 +1,36 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Card } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 
 const SleeperCard = ({ player }) => {
   return (
-    <Card
-      title={player.name}
-      hoverable
-      loading={!player ? true : false}
-      extra={<Link href={`/players/${player._id}`}><PlusOutlined /></Link>}
-    >
-      <Meta
-        description={player.sleeper[1]}
-        avatar={
-          <Image
-            src={player.imgUrl}
-            alt={player.name}
-            width={100}
-            height={100}
-            style={{borderRadius: '60%', backgroundColor: 'rgba(0,0,0,0.09)'}}
-          />
-        }
-      />
-    </Card>
+    <Link href={`/players/${player._id}`}>
+      <Card
+        title={player.name}
+        hoverable="true"
+        type="inner"
+        loading={!player ? true : false}
+        extra={player.position}
+      >
+        <Meta
+          description={player.sleeper[1]}
+          avatar={
+            <Image
+              src={player.imgUrl}
+              alt={player.name}
+              width={100}
+              height={100}
+              style={{
+                borderRadius: "60%",
+                backgroundColor: "rgba(0,0,0,0.09)",
+              }}
+            />
+          }
+        />
+      </Card>
+    </Link>
   );
 };
 
