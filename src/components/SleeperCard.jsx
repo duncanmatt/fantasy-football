0f54d1ee8431d2790acd3eb37aa7b70639ecd58e
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-04-24 00:18:47 -0400

    sleeper card headshot fill

--- src/components/SleeperCard.jsx
+++ src/components/SleeperCard.jsx
@@ -1,47 +1,53 @@
-import Link from "next/link";
-import Image from "next/image";
-import { FileSearchOutlined } from "@ant-design/icons";
-import styles from "../styles/Home.module.css";
+import Link from 'next/link';
+import Image from 'next/image';
+import { FileSearchOutlined } from '@ant-design/icons';
+import styles from '../styles/Home.module.css';
 
 const SleeperCard = ({ player }) => {
-  return (
-    <div className={styles.sleeper}>
-      <div className={styles.sleeperUpper}>
-        <h4>{player.name}</h4>
-        <Link className={styles.sleeperLink} href={`/players/${player._id}`}>
-          <FileSearchOutlined />
-        </Link>
-      </div>
-      <div className={styles.sleeperBody}>
-        <span className={styles.sleeperPos}>{player.position}</span>
-        <div className={styles.sleeperHeadshotWrapper}>
-          <picture>
-            <Image
-              className={styles.sleeperHeadshot}
-              src={player.imgUrl}
-              alt={player.name}
-              height={200}
-              width={200}
-              style={{
-                // borderRadius: "50%",
-              }}
-            />
-          </picture>
-        </div>
-        <Image
-          className={styles.sleeperLogo}
-          width={40}
-          height={40}
-          src={player.team_logo}
-          alt={player.team}
-        />
-      </div>
-      <div className={styles.sleeperOutlook}>
-        <span>{player.sleeper[1]}</span>
-      </div>
-    </div>
-
-  );
+	return (
+		<div className={styles.sleeper}>
+			<div className={styles.sleeperUpper}>
+				<h4>{player.name}</h4>
+				<div style={{ display: 'flex', alignItems: 'center' }}>
+					<span className={styles.sleeperPos}>{player.position}</span>
+					<Image
+						className={styles.sleeperLogo}
+						width={40}
+						height={40}
+						src={player.team_logo}
+						alt={player.team}
+					/>
+				</div>
+			</div>
+			<div className={styles.sleeperBody}>
+				<div className={styles.sleeperHeadshotWrapper}>
+					<picture>
+						<Image
+							className={styles.sleeperHeadshot}
+							src={player.imgUrl}
+							alt={player.name}
+							fill
+							style={
+								{
+									// borderRadius: "50%",
+								}
+							}
+						/>
+					</picture>
+				</div>
+			</div>
+			<div className={styles.sleeperOutlook}>
+				<span>{player.sleeper[1]}</span>
+			</div>
+			<div className={styles.sleeperLinkWrapper}>
+				<Link
+					className={styles.sleeperLink}
+					href={`/players/${player._id}`}>
+					STATS
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default SleeperCard;
