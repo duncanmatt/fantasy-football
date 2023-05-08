143d017e900a391c463c9ef61eb3bee8891085df
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-05-07 17:49:47 -0700

    formatted index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,47 @@
-import connectDB from '../../lib/connectDB';
-import Player from '../../models/Player';
-import Transactions from '../components/Transactions';
-import News from '../components/News';
-import Sleepers from '../components/Sleepers';
-import Layout from '../components/Layout';
-import { Divider, Spin } from 'antd';
-import styles from '../styles/Home.module.css';
+import connectDB from "../../lib/connectDB";
+import Player from "../../models/Player";
+import Transactions from "../components/Transactions";
+import News from "../components/News";
+import Sleepers from "../components/Sleepers";
+import Layout from "../components/Layout";
+import { Divider, Spin } from "antd";
+import styles from "../styles/Home.module.css";
 
 const Home = ({ sleepers }) => {
-	return (
-		<Layout>
-			<main className={styles.main}>
-				<section className={styles.landing}>
-					<Transactions />
-					<div className={styles.newsWrapper}>
-						<div className={styles.heading}>Latest <span className={styles.dNews}>News</span></div>
-						<News />
-						<Divider style={{backgroundColor: 'rgb(217,217,217'}} />
-					</div>
-				</section>
-				<div className={styles.sleepersWrapper}>
-					<div className={styles.heading}>Sleepers</div>
-					{sleepers ? <Sleepers players={sleepers} /> : <Spin />}
-				</div>
-			</main>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <main className={styles.main}>
+        <section className={styles.landing}>
+          <Transactions />
+          <div className={styles.newsWrapper}>
+            <div className={styles.heading}>
+              Latest <span className={styles.dNews}>News</span>
+            </div>
+            <News />
+            <Divider style={{ backgroundColor: "rgb(217,217,217" }} />
+          </div>
+        </section>
+        <div className={styles.sleepersWrapper}>
+          <div className={styles.heading}>Sleepers</div>
+          {sleepers ? <Sleepers players={sleepers} /> : <Spin />}
+        </div>
+      </main>
+    </Layout>
+  );
 };
 
 export async function getStaticProps() {
-	await connectDB();
+  await connectDB();
 
-	const sleepersResult = await Player.find({ sleeper: true });
+  const sleepersResult = await Player.find({ sleeper: true });
 
-	const sleepers = sleepersResult.map(doc => {
-		const sleeper = doc.toObject();
-		sleeper._id = sleeper._id.toString();
-		return sleeper;
-	});
+  const sleepers = sleepersResult.map((doc) => {
+    const sleeper = doc.toObject();
+    sleeper._id = sleeper._id.toString();
+    return sleeper;
+  });
 
-	return { props: { sleepers: sleepers } };
+  return { props: { sleepers: sleepers } };
 }
 
 export default Home;
