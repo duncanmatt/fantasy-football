f73270f311931901b6d5895cdc21986e9d3a7a49
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-16 20:33:55 -0400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,24 @@
-import logo from "../../public/logo.svg";
-import { Divider } from "antd";
-import Image from "next/image";
+import logo from '../../public/logo.svg';
+import { Divider } from 'antd';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <Divider style={{backgroundColor: '#000'}} />
+    <div className='footer'>
+      <Divider style={{ backgroundColor: '#000' }} />
       <div
         style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          paddingBlockStart: '1rem'
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingBlockStart: '1rem',
         }}
       >
-        &copy; 2023 Offseason Advisor
+        &copy; 2023, Fantasy Football Auxiliary
       </div>
       <p>stats from NFL.com</p>
-      <Image src={logo} alt="Offseason Advisor" width={30} height={30} />
+      <Image src={logo} alt='Offseason Advisor' width={30} height={30} />
     </div>
   );
 };
