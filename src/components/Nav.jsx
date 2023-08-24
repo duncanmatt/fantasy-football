c7efaae65564bb3e3a978862735acb7d165bbe11
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-24 19:02:07 -0400

    selection bg

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,137 +4,118 @@ import { useState } from 'react';
 import Image from 'next/image';
 import logo from '../../public/logo.svg';
 import {
-	MenuOutlined,
-	UserOutlined,
-	CloseOutlined,
-	RightOutlined,
+  MenuOutlined,
+  UserOutlined,
+  CloseOutlined,
+  RightOutlined,
 } from '@ant-design/icons';
 import styles from '../styles/Nav.module.css';
 
 function MobileNav() {
-	const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-	const showMenu = () => setMenuOpen(!menuOpen);
+  const showMenu = () => setMenuOpen(!menuOpen);
 
-	return (
-		<div className={styles.mobileWrapper}>
-			{menuOpen ? (
-				<>
-					<div className={styles.mobile}>
-						<span>
-							<Link href='/'>
-								<Image
-									src={logo}
-									alt='Fantasy Football Auxiliary'
-									width={36}
-									height={36}
-								/>
-							</Link>
-						</span>
-						<span className={styles.actions}>
-							<CloseOutlined onClick={showMenu} />
-						</span>
-					</div>
-					<nav className={styles.menu}>
-						<div className={styles.mobileLinks}>
-							<Link
-								className={styles.pos}
-								href='/QBs'>
-								QUARTER BACKS <RightOutlined />
-							</Link>
-							<Divider />
-							<Link
-								className={styles.pos}
-								href='/RBs'>
-								RUNNING BACKS <RightOutlined />
-							</Link>
-							<Divider />
-							<Link
-								className={styles.pos}
-								href='/WRs'>
-								WIDE RECEIVERS <RightOutlined />
-							</Link>
-							<Divider />
-							<Link
-								className={styles.pos}
-								href='/TEs'>
-								TIGHT ENDS <RightOutlined />
-							</Link>
-							<Divider />
-							<span className={styles.accountWrapper}>
-								<h4>Account</h4>
-								<span className={styles.account}>
-									<Link
-										href='/Profile'
-										className={styles.login}>
-										Login
-									</Link>
-									<Link
-										href='/Register'
-										className={styles.create}>
-										Create Account
-									</Link>
-								</span>
-							</span>
-						</div>
-					</nav>
-				</>
-			) : (
-				<div className={styles.mobile}>
-					<span>
-						<Link
-							href='/'
-							className={styles.title}>
-							<Image
-								src={logo}
-								alt='Fantasy Football Auxiliary'
-								width={36}
-								height={36}
-							/>
-						</Link>
-					</span>
-					<span className={styles.actions}>
-						<MenuOutlined style={{lineHeight: '1', verticalAlign: '0', paddingTop: '5px'}} onClick={showMenu} />
-					</span>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className={styles.mobileWrapper}>
+      {menuOpen ? (
+        <>
+          <div style={{ backgroundColor: '#f9f9f9' }} className={styles.mobile}>
+            <span></span>
+            <span className={styles.actions}>
+              <CloseOutlined onClick={showMenu} />
+            </span>
+          </div>
+          <nav className={styles.menu}>
+            <div className={styles.mobileLinks}>
+              <Link className={styles.pos} href='/QBs'>
+                QUARTER BACKS <RightOutlined />
+              </Link>
+              <Divider />
+              <Link className={styles.pos} href='/RBs'>
+                RUNNING BACKS <RightOutlined />
+              </Link>
+              <Divider />
+              <Link className={styles.pos} href='/WRs'>
+                WIDE RECEIVERS <RightOutlined />
+              </Link>
+              <Divider />
+              <Link className={styles.pos} href='/TEs'>
+                TIGHT ENDS <RightOutlined />
+              </Link>
+              <Divider />
+              <span className={styles.accountWrapper}>
+                <h4>Account</h4>
+                <span className={styles.account}>
+                  <Link href='/Profile' className={styles.login}>
+                    Login
+                  </Link>
+                  <Link href='/Register' className={styles.create}>
+                    Create Account
+                  </Link>
+                </span>
+              </span>
+            </div>
+          </nav>
+        </>
+      ) : (
+        <div className={styles.mobile}>
+          <span>
+            <Link href='/' className={styles.title}>
+              <Image
+                src={logo}
+                alt='Fantasy Football Auxiliary'
+                width={36}
+                height={36}
+              />
+            </Link>
+          </span>
+          <span className={styles.actions}>
+            <MenuOutlined onClick={showMenu} />
+          </span>
+        </div>
+      )}
+    </div>
+  );
 }
 
 const Nav = () => {
-	return (
-		<>
-			<header className='header-wrapper'>
-				<div className='header-content'>
-					<MobileNav />
-					<div className={styles.desktop}>
-						<span>
-							<Link
-								className={styles.title}
-								href='/'>
-								<Image
-									src={logo}
-									alt='Fantasy Football Auxiliary'
-									width={36}
-									height={36}
-								/>
-							</Link>
-						</span>
-						<nav className={styles.links}>
-							<Link href='/QBs'>QB</Link>
-							<Link href='/RBs'>RB</Link>
-							<Link href='/WRs'>WR</Link>
-							<Link href='/TEs'>TE</Link>
-							<Link href='/Profile'>
-								<UserOutlined style={{lineHeight: '1', verticalAlign: '0', paddingTop: '5px'}} />
-							</Link>
-						</nav>
-					</div>
-				</div>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header className='header-wrapper'>
+        <div className='header-content'>
+          <MobileNav />
+          <div className={styles.desktop}>
+            <span>
+              <Link className={styles.title} href='/'>
+                <Image
+                  src={logo}
+                  alt='Fantasy Football Auxiliary'
+                  width={36}
+                  height={36}
+                />
+              </Link>
+            </span>
+            <nav className={styles.links}>
+              <Link href='/QBs'>QB</Link>
+              <Link href='/RBs'>RB</Link>
+              <Link href='/WRs'>WR</Link>
+              <Link href='/TEs'>TE</Link>
+              <Link href='/Profile'>
+                <UserOutlined
+                  style={{
+                    lineHeight: '1',
+                    verticalAlign: '0',
+                    paddingTop: '5px',
+                  }}
+                />
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Nav;
