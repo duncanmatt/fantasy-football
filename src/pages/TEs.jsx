3e5c140624646f9e7d05042a4a0dd92fdd739cee
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-24 20:35:27 -0400

    fix wr and te pages by setting player move to null

--- src/pages/TEs.jsx
+++ src/pages/TEs.jsx
@@ -1,60 +1,60 @@
-import connectDB from "../../lib/connectDB";
-import Player from "../../models/Player";
-import Layout from "../components/Layout";
-import Link from "next/link";
-import { Table } from "antd";
-import styles from "../styles/Position.module.css";
+import connectDB from '../../lib/connectDB';
+import Player from '../../models/Player';
+import Layout from '../components/Layout';
+import Link from 'next/link';
+import { Table } from 'antd';
+import styles from '../styles/Position.module.css';
 
 const TEs = ({ players }) => {
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
+      title: 'Name',
+      dataIndex: 'name',
       render: (text, record) => (
         <Link href={`/players/${record._id}`}>{text}</Link>
       ),
     },
     {
-      title: "Position",
-      dataIndex: "position",
+      title: 'Position',
+      dataIndex: 'position',
     },
     {
-      title: "Receiving",
+      title: 'Receiving',
       children: [
         {
-          title: "Yards",
-          dataIndex: "rec_yards",
+          title: 'Yards',
+          dataIndex: 'rec_yards',
         },
         {
-          title: "Targets",
-          dataIndex: "targets",
+          title: 'Targets',
+          dataIndex: 'targets',
         },
         {
-          title: "Receptions",
-          dataIndex: "receptions",
+          title: 'Receptions',
+          dataIndex: 'receptions',
         },
         {
-          title: "TDs",
-          dataIndex: "rec_TDs",
+          title: 'TDs',
+          dataIndex: 'rec_TDs',
         },
       ],
     },
     {
-      title: "Rushing",
+      title: 'Rushing',
       children: [
         {
-          title: "Yards",
-          dataIndex: "run_yards",
+          title: 'Yards',
+          dataIndex: 'run_yards',
         },
         {
-          title: "TDs",
-          dataIndex: "run_TDs",
+          title: 'TDs',
+          dataIndex: 'run_TDs',
         },
       ],
     },
     {
-      title: "FUMs",
-      dataIndex: "FUMs",
+      title: 'FUMs',
+      dataIndex: 'FUMs',
     },
   ];
 
@@ -62,14 +62,14 @@ const TEs = ({ players }) => {
     <Layout>
       <div className={styles.main}>
         <h2 className={styles.title}>TIGHT ENDS</h2>
-          <Table
-            bordered={true}
-            scroll={{x: true}}
-            dataSource={players}
-            columns={columns}
-            rowKey={(record) => record._id}
-          />
-        </div>
+        <Table
+          bordered={true}
+          scroll={{ x: true }}
+          dataSource={players}
+          columns={columns}
+          rowKey={(record) => record._id}
+        />
+      </div>
     </Layout>
   );
 };
@@ -77,10 +77,11 @@ const TEs = ({ players }) => {
 export async function getServerSideProps(context) {
   await connectDB();
 
-  const playersResult = await Player.find({ position: "TE" });
+  const playersResult = await Player.find({ position: 'TE' });
   const players = playersResult.map((doc) => {
     const player = doc.toObject();
     player._id = player._id.toString();
+    player.move = null;
     return player;
   });
 
--- src/pages/WRs.jsx
+++ src/pages/WRs.jsx
@@ -6,85 +6,86 @@ import { Table } from 'antd';
 import styles from '../styles/Position.module.css';
 
 const WRs = ({ players }) => {
-	const columns = [
-		{
-			title: 'Name',
-			dataIndex: 'name',
-			render: (text, record) => (
-				<Link href={`/players/${record._id}`}>{text}</Link>
-			),
-		},
-		{
-			title: 'Position',
-			dataIndex: 'position',
-		},
-		{
-			title: 'Receiving',
-			children: [
-				{
-					title: 'Yards',
-					dataIndex: 'rec_yards',
-				},
-				{
-					title: 'Target',
-					dataIndex: 'targets',
-				},
-				{
-					title: 'Receptions',
-					dataIndex: 'receptions',
-				},
-				{
-					title: 'TDs',
-					dataIndex: 'rec_TDs',
-				},
-			],
-		},
-		{
-			title: 'Rushing',
-			children: [
-				{
-					title: 'Yards',
-					dataIndex: 'run_yards',
-				},
-				{
-					title: 'TDs',
-					dataIndex: 'run_TDs',
-				},
-			],
-		},
-		{
-			title: 'FUMs',
-			dataIndex: 'FUMs',
-		},
-	];
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      render: (text, record) => (
+        <Link href={`/players/${record._id}`}>{text}</Link>
+      ),
+    },
+    {
+      title: 'Position',
+      dataIndex: 'position',
+    },
+    {
+      title: 'Receiving',
+      children: [
+        {
+          title: 'Yards',
+          dataIndex: 'rec_yards',
+        },
+        {
+          title: 'Target',
+          dataIndex: 'targets',
+        },
+        {
+          title: 'Receptions',
+          dataIndex: 'receptions',
+        },
+        {
+          title: 'TDs',
+          dataIndex: 'rec_TDs',
+        },
+      ],
+    },
+    {
+      title: 'Rushing',
+      children: [
+        {
+          title: 'Yards',
+          dataIndex: 'run_yards',
+        },
+        {
+          title: 'TDs',
+          dataIndex: 'run_TDs',
+        },
+      ],
+    },
+    {
+      title: 'FUMs',
+      dataIndex: 'FUMs',
+    },
+  ];
 
-	return (
-		<Layout>
-			<div className={styles.main}>
-				<h2 className={styles.title}>WIDE RECEIVERS</h2>
-				<Table
-					bordered={true}
-					scroll={{x: true}}
-					dataSource={players}
-					columns={columns}
-					rowKey={record => record._id}
-				/>
-			</div>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <div className={styles.main}>
+        <h2 className={styles.title}>WIDE RECEIVERS</h2>
+        <Table
+          bordered={true}
+          scroll={{ x: true }}
+          dataSource={players}
+          columns={columns}
+          rowKey={(record) => record._id}
+        />
+      </div>
+    </Layout>
+  );
 };
 
 export async function getServerSideProps(context) {
-	await connectDB();
+  await connectDB();
 
-	const playersResult = await Player.find({ position: 'WR' });
-	const players = playersResult.map(doc => {
-		const player = doc.toObject();
-		player._id = player._id.toString();
-		return player;
-	});
+  const playersResult = await Player.find({ position: 'WR' });
+  const players = playersResult.map((doc) => {
+    const player = doc.toObject();
+    player._id = player._id.toString();
+    player.move = null;
+    return player;
+  });
 
-	return { props: { players: players } };
+  return { props: { players: players } };
 }
 
 export default WRs;
