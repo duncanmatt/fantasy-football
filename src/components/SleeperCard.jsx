2bc64bbe474ec7500f7a81f67ad64bbc8d6f2150
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-09-13 19:50:24 -0400

    sleeper card layout

--- src/components/SleeperCard.jsx
+++ src/components/SleeperCard.jsx
@@ -12,23 +12,23 @@ const SleeperCard = ({ player }) => {
           right: '0',
           top: '1.45rem',
         }}
-      >
+      ></div>
+      <div className={styles.sleeperBody}>
+        <div className={styles.sleeperUpper}>
+          <h4 className={styles.sleeperName}>{player.name}</h4>
+          <span className={styles.sleeperPos}>{player.position}</span>
+        </div>
         <span className={styles.sleeperLogoWrapper}>
           <Image
             className={styles.sleeperLogo}
-            fill
-            src={player.team_logo}
             alt={player.team}
+            src={`https://static.www.nfl.com/league/apps/fantasy/logos/200x213/${player.team_abv}.png`}
+            fill
+            sizes='(max-width: 600px) calc(100vw - 2rem),	(min-width: 600px) minmax(200px, 1fr), (min-width: 1100px) 30vw, (min-width: 1400px) 20vw'
             placeholder='blur'
             blurDataURL='data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
           />
         </span>
-      </div>
-      <div className={styles.sleeperUpper}>
-        <h4 className={styles.sleeperName}>{player.name}</h4>
-        <span className={styles.sleeperPos}>{player.position}</span>
-      </div>
-      <div className={styles.sleeperBody}>
         <div className={styles.sleeperHeadshotWrapper}>
           <Image
             className={styles.sleeperHeadshot}
@@ -41,8 +41,17 @@ const SleeperCard = ({ player }) => {
           />
         </div>
       </div>
+      <div className={styles.sleeperDetails}>
+        <div className={styles.sleeperDetailsInner}>
+          <span className={styles.sleeperDetailsTeam}>{player.team}</span>
+          <span className={styles.sleeperDetailsNumber}>#{player.num}</span>
+        </div>
+      </div>
       <div className={styles.sleeperOutlook}>
-        <span>{player.sleeper[1]}</span>
+        <span className={styles.sleeperOutlookHeader}>
+          <h6>2023 outlook</h6>
+        </span>
+        <span className={styles.sleeperOutlookDesc}>{player.sleeper[1]}</span>
       </div>
       <div className={styles.sleeperLinkWrapper}>
         <Link className={styles.sleeperLink} href={`/players/${player._id}`}>
