1ff1166f2145ac4d4753d6c22c19fbcf794fe80d
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-11-09 20:55:05 -0500

    update footer credits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import logo from '../../public/logo.svg';
 import { Divider } from 'antd';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -17,7 +18,21 @@ const Footer = () => {
       >
         &copy; 2023, Fantasy Football Auxiliary
       </div>
-      <p>stats from NFL.com</p>
+      <div>
+        <p>
+          stats compiled from{' '}
+          <Link
+            href='https://www.fantasypros.com'
+            style={{ textDecoration: 'underline' }}
+          >
+            Fantasy Pros
+          </Link>{' '}
+          and{' '}
+          <Link style={{ textDecoration: 'underline' }} href='https://nfl.com'>
+            NFL
+          </Link>
+        </p>
+      </div>
       <Image src={logo} alt='Offseason Advisor' width={30} height={30} />
     </div>
   );
