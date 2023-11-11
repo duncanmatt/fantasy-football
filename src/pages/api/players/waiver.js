ec7fbd709184e7dd5347c7df29ca9633e4aa83f7
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-11-11 02:07:30 -0500

    fix css on large viewports

--- src/pages/api/players/waiver.js
+++ src/pages/api/players/waiver.js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
       if (!response) {
         return res.status(400).json({ success: false });
       }
-      console.log(response);
       res.status(200).send(response);
     } catch (error) {
       res.status(400).json({ success: false, message: error });
