23fc3212072dba4fddcc2773e855dee0baff5f61
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-11-11 06:48:12 -0500

    ssg with swr cache

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import Transactions from '../components/Transactions';
+import { getJson } from 'serpapi';
 import News from '../components/News';
 import WaiverWinners from '../components/WaiverWinners';
 import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
+import { SWRConfig } from 'swr';
 
-const Home = ({ articles }) => {
+const Home = ({ articles, fallback }) => {
   return (
-    <>
+    <SWRConfig value={{ provider: () => new Map(), fallback }}>
       <Layout>
         <main className={styles.main}>
           <section className={styles.landing}>
@@ -24,25 +26,35 @@ const Home = ({ articles }) => {
           </div>
         </main>
       </Layout>
-    </>
+    </SWRConfig>
   );
 };
 
-export async function getServerSideProps(ctx) {
-  const res = await fetch(
-    process.env.NODE_ENV === 'development'
-      ? `http://${ctx.req?.headers.host}/api/news`
-      : `https://${ctx.req?.headers.host}/api/news`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
-  );
-  const data = await res.json();
-
-  return { props: { articles: data } };
-}
-
 export default Home;
+
+export async function getStaticProps(ctx) {
+  const newsParams = {
+    api_key: process.env.SERP_API_KEY,
+    q: 'nfl today',
+    gl: 'us',
+    hl: 'en',
+    ijn: '0',
+    tbm: 'nws',
+  };
+
+  const res = await getJson('google', newsParams);
+
+  const data = await res.news_results;
+
+  const url =
+    'https://www.google.com/search?q=nfl+today&oq=nfl+today&hl=en&gl=us&tbm=nws&sourceid=chrome&ie=UTF-8';
+
+  return {
+    props: {
+      articles: data,
+      fallback: {
+        [url]: data,
+      },
+    },
+  };
+}
